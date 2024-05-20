ca728f73e6af3a0f00687a0c53e0e901d74624fa
s22078-adriannagabrys <127010785+s22078-adriannagabrys@users.noreply.github.com>
2024-05-20 17:09:38 +0200

    Update app.py

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -2,6 +2,7 @@ import base64
 import streamlit as st
 from PIL import ImageOps, Image
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 
 class_names = ["Curved spine", "Dead", "Edema", "Normal", "Unhatched", "Yolk deformation"]
@@ -29,6 +30,7 @@ def set_background(image_file):
     """
     st.markdown(style, unsafe_allow_html=True)
 
+
 def classify(image, model, class_names):
     """
     This function takes an image, a model, and a list of class names and returns the predicted class and confidence
@@ -40,56 +42,58 @@ def classify(image, model, class_names):
         class_names (list): A list of class names corresponding to the classes that the model can predict.
 
     Returns:
-        numpy.ndarray: The prediction array.
+        A tuple of the predicted class name and the confidence score for that prediction.
     """
-    # Convert image to (224, 224)
+    # convert image to (224, 224)
     image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
 
-    # Convert image to numpy array
+    # convert image to numpy array
     image_array = np.asarray(image)
 
-    # Normalize image
+    # normalize image
     normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-    # Set model input
+    # set model input
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     data[0] = normalized_image_array
 
-    # Make prediction
+    # make prediction
     pred = model.predict(data)
 
     return pred
 
-# Set title
-st.title('Zebrafish Malformations Classification')
 
-# Set header
+# set title
+st.title('Zebrafish malformations classification')
+
+# set header
 st.header('Please upload a Zebrafish larvae image')
 
-# Upload file
+# upload file
 file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 
-
+# load classifier
 model = tf.keras.models.load_model("pythonProject/ResNet_BestSoFar.h5", compile=False)
 
-# Display image and classify if a file is uploaded and model is loaded
-if file is not None and model is not None:
-    try:
-        image = Image.open(file).convert('RGB')
-        st.image(image, use_column_width=True)
+# display image
+if file is not None:
+    image = Image.open(file).convert('RGB')
+    st.image(image, use_column_width=True)
 
-        # Classify image
-        pred = classify(image, model, class_names)
+    # classify image
+    pred = classify(image, model, class_names)
 
-        # Convert prediction to numpy array
-        pred = np.array(pred)
+    pred = np.array(pred)
 
-        # Display prediction results
-        st.write("Prediction Results:", pred)
-    except Exception as e:
-        st.error(f"Error during classification: {e}")
-else:
-    if model is None:
-        st.error("Model is not loaded.")
-    else:
-        st.info("Please upload an image file.")
+    sorted_indices = np.argsort(pred[0])[::-1]  # Sort indices in descending order based on pred[0]
+
+    sorted_class_names = [class_names[i] for i in sorted_indices]
+    sorted_pred = [pred[0][i] for i in sorted_indices]
+
+
+    # Create a DataFrame to hold the sorted data
+    data = {"Class Name": sorted_class_names, "Prediction (%)": [f"{p * 100:.2f}%" for p in sorted_pred]}
+    df = pd.DataFrame(data)
+
+    # Display the DataFrame as a table with invisible borders
+    st.write(df)
