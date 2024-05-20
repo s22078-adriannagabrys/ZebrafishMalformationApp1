bb6af549aaa0e4eabed95be5422e12a54c83db12
s22078-adriannagabrys <127010785+s22078-adriannagabrys@users.noreply.github.com>
2024-05-20 17:36:52 +0200

    Update app.py

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from PIL import ImageOps, Image
 import numpy as np
 import tensorflow as tf
-import matplotlib.cm as cm
+from matplotlib.pyplot import cm
 
 # Define class names
 class_names = ["Curved spine", "Dead", "Edema", "Normal", "Unhatched", "Yolk deformation"]
