1830a5e80941e14b0abc1a08533ae6a31ce7c8d2
s22078-adriannagabrys <127010785+s22078-adriannagabrys@users.noreply.github.com>
2024-05-20 16:57:16 +0200

    Update app.py

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -4,15 +4,19 @@ from PIL import ImageOps, Image
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import matplotlib.pyplot as plt
-import os
-
-# Reduce TensorFlow logging
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 class_names = ["Curved spine", "Dead", "Edema", "Normal", "Unhatched", "Yolk deformation"]
 
 def set_background(image_file):
+    """
+    This function sets the background of a Streamlit app to an image specified by the given image file.
+
+    Parameters:
+        image_file (str): The path to the image file to be used as the background.
+
+    Returns:
+        None
+    """
     with open(image_file, "rb") as f:
         img_data = f.read()
     b64_encoded = base64.b64encode(img_data).decode()
@@ -26,79 +30,74 @@ def set_background(image_file):
     """
     st.markdown(style, unsafe_allow_html=True)
 
+
 def classify(image, model, class_names):
-    try:
-        # Clear Keras session to free up resources
-        tf.keras.backend.clear_session()
+    """
+    This function takes an image, a model, and a list of class names and returns the predicted class and confidence
+    score of the image.
 
-        # Convert image to (224, 224)
-        image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
+    Parameters:
+        image (PIL.Image.Image): An image to be classified.
+        model (tensorflow.keras.Model): A trained machine learning model for image classification.
+        class_names (list): A list of class names corresponding to the classes that the model can predict.
 
-        # Convert image to numpy array
-        image_array = np.asarray(image)
+    Returns:
+        A tuple of the predicted class name and the confidence score for that prediction.
+    """
+    # convert image to (224, 224)
+    image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
 
-        # Normalize image
-        normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+    # convert image to numpy array
+    image_array = np.asarray(image)
 
-        # Set model input
-        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-        data[0] = normalized_image_array
+    # normalize image
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-        # Make prediction
-        pred = model.predict(data)
+    # set model input
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    data[0] = normalized_image_array
 
-        # Ensure the prediction is a numpy array
-        pred = np.array(pred)
+    # make prediction
+    pred = model.predict(data)
 
-        return pred
+    return pred
 
-    except Exception as e:
-        st.error(f"Error during classification: {e}")
-        return None
 
-# Set title
-st.title('Zebrafish Malformations Classification')
+# set title
+st.title('Zebrafish malformations classification')
 
-# Set header
+# set header
 st.header('Please upload a Zebrafish larvae image')
 
-# Upload file
+# upload file
 file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 
-# Load classifier
-try:
-    model = tf.keras.models.load_model("pythonProject/ResNet_BestSoFar.h5", compile=False)
-except Exception as e:
-    st.error(f"Error loading model: {e}")
-    model = None
+# load classifier
+model = tf.keras.models.load_model("pythonProject/ResNet_BestSoFar.h5", compile=False)
 
-# Display image
-if file is not None and model is not None:
+# display image
+if file is not None:
     image = Image.open(file).convert('RGB')
     st.image(image, use_column_width=True)
 
-    # Classify image
+    # classify image
     pred = classify(image, model, class_names)
 
-    if pred is not None and len(pred) > 0:
-        sorted_indices = np.argsort(pred[0])[::-1]  # Sort indices in descending order based on pred[0]
+    pred = np.array(pred)
 
-        sorted_class_names = [class_names[i] for i in sorted_indices]
-        sorted_pred = [pred[0][i] for i in sorted_indices]
+    sorted_indices = np.argsort(pred[0])[::-1]  # Sort indices in descending order based on pred[0]
 
-        filtered_data = {"Class Name": [], "Prediction (%)": []}
-        for class_name, prediction in zip(sorted_class_names, sorted_pred):
-            if prediction > 0.5:
-                filtered_data["Class Name"].append(class_name)
-                filtered_data["Prediction (%)"].append(f"{prediction * 100:.2f}%")
+    sorted_class_names = [class_names[i] for i in sorted_indices]
+    sorted_pred = [pred[0][i] for i in sorted_indices]
 
-        # Create a DataFrame to hold the filtered data
-        filtered_df = pd.DataFrame(filtered_data)
-
-        # Display the filtered DataFrame as a table with invisible borders
-        st.write(filtered_df)
-    else:
-        st.error("Failed to classify the image.")
-else:
-    if model is None:
-        st.error("Model is not loaded.")
+    filtered_data = {"Class Name": [], "Prediction (%)": []}
+    for class_name, prediction in zip(sorted_class_names, sorted_pred):
+        if prediction > 0.5:
+            filtered_data["Class Name"].append(class_name)
+            filtered_data["Prediction (%)"].append(f"{prediction * 100:.2f}%")
+    
+    # Create a DataFrame to hold the filtered data
+    filtered_df = pd.DataFrame(filtered_data)
+    
+    # Display the filtered DataFrame as a table with invisible borders
+    st.write(filtered_df)
